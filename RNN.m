a57bbe3a56ecd44f0a7d6ab82826036715b5369a
サム <samuelph@kth.se>
2017-06-11 14:05:46 +0200

    second commit

--- RNN.m
+++ RNN.m
@@ -31,9 +31,32 @@ classdef RNN < matlab.mixin.SetGet
       %  h0 (the hidden state at time 0)
       %  x0 represent the first (dummy) input
       %  n denotes the length of the sequence you want to generate
+      h{1} = h0;
+      x = x0;
+      for i = 1:size(x,2)
+        a{i} = obj.W * h{i} + obj.U * x(:,i) + obj.b;
+        h{i+1} = tanh(a{i});
+        o{i} = V*h{i+1} + c;
+        p{i} = SOFTMAX({o{i}});
+      end
+    end
+    function ret = SOFTMAX(P)
+      e = exp(P);
+      one = ones(size(P,1),1);
+      split = one'*e;
+      ret = e/split(1,1);
     end
     function ii = randCharSelect(obj, p,cp)
-
+      cp = cumsum(p);
+      a = rand;
+      ixs = find(cp-a >0);
+      ii = ixs(1);
+    end
+    function cost_val = cost(Y, P)
+      cost_val = 0;
+      for i = size(Y,2)
+        cost_val = cost_val - log(Y(:,i)'*P(:,i));
+      end
     end
   end
 end
